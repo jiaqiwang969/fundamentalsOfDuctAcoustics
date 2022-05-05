6850b2f6d530879147e8f7a8f9cc6c38afc80ccc
jiaqiwang969 <jiaqiwang969@gmail.com>
2022-05-05 23:29:56 +0800

    -

--- Fig5_deriv/Fig5_deriv06.m
+++ Fig5_deriv/Fig5_deriv06.m
@@ -18,7 +18,7 @@ addpath(genpath('../subfunction'));
 %% Sensor Positions
 N = 100;
 sensor.z0 = 8;
-sensor.z = sensor.z0*ones(N,1);  % 
+sensor.z = sensor.z0*ones(N,1);  %
 sensor.rho = ones(N,1); % all sensor in the wall
 sensor.theta = 2*pi/N*[1:N].';
 [sensor.x,sensor.y,sensor.z] = pol2cart(sensor.theta,sensor.rho,sensor.z);
@@ -31,22 +31,25 @@ Zs = Zs*h;
 %% Mode Generator
 c = 343;               % sound speed
 m = [0:17];              % circumferential modes maker
-n = [1];
+n = [1:7];
 % f=[1000];            % physical frequency %f*2*pi*rT/c-0.0000000001*i;
 [Base] = BaseJ1(m,n(end),rT);  %Rienstra-50, at Least n-radialModes calculated
 
-w=linspace(0,20,10000);
-for k=1:length(w)
-    Eig(k,:) = real(sqrt(w(k)^2-Base.jmn_pm.^2)); %Rienstra-52
-end
-
+w=linspace(0,20,1000);
 
 
 figure
-for k=1:length(m)
-   scatter(w,Eig(:,k),'.');
-   hold on 
+for kn=1:length(n)
+    for k=1:length(w)
+        Eig(k,:) = real(sqrt(w(k)^2-Base.jmn_pm(n(kn),:).^2)); %Rienstra-52
+    end
+
+    for k=1:length(m)
+        scatter3(w,Eig(:,k),n(kn)*ones(size(w)),'.');
+        hold on
+    end
 end
+
 ylabel({'Re(k_{m1})'});
 xlabel({'w'});
 title({'Figure-08'});
